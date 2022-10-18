60061d3300f7b2ac95f448cb4d733201086f42c5
Santiago Fiorotto <santifiorotto@hotmail.com>
2022-10-18 01:13:16 -0300

    Primer Cambio

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {TiShoppingCart} from "react-icons/ti"
+
+const CartWidget = () => {
+  return (
+   <div className='carrito'>
+    <TiShoppingCart/>
+    </div>
+  )
+}
+
+export default CartWidget
