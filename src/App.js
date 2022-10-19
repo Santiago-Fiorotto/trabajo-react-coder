a434f9a6547329453d3e54c601592fdcc813cef7
Santiago Fiorotto <santifiorotto@hotmail.com>
2022-10-19 20:56:38 -0300

    PreEntrega1+Fiorotto

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer greeting= "Bienvenidos a mi sitio de Ecommerce !!"/>
+      <ItemListContainer greeting= "Bienvenidos a mi sitio de Ecommerce !"/>
     </div>
   );
 }
