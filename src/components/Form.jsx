8d17bf63ed9ce03981313b8dc992b6814c59cf26
Santiago Fiorotto <santifiorotto@hotmail.com>
2022-11-24 19:48:44 -0300

    formulario completo y con validacion

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,6 +8,10 @@ import { db } from '../services/firebaseConfig';
 const Form = () => {
     const [name,setName] = useState ("");
     const [lastName,setLastName] = useState ("");
+    const [telefono, setTelefono] = useState();
+    const [direccion, setDireccion] = useState ("");
+    const [mailUno, setMailUno] = useState ("");
+    const [mailDos, setMailDos] = useState ("");
     const [orderId, setOrderId] = useState("");
 
     const {cart, totalPrecio, deleteAll} = useContext(CartContext);
@@ -48,6 +52,24 @@ const Form = () => {
         setLastName (e.target.value)
     };
 
+    const handleTelefono = (e) => {
+      setTelefono (e.target.value)
+    };
+
+    const handleDireccion = (e) => {
+      setDireccion (e.target.value)
+  };
+
+  const handleMailUno = (e) => {
+    setMailUno(e.target.value)
+};
+
+const handleMailDos = (e) => {
+  setMailDos (e.target.value)
+};
+
+
+
     if (orderId) {
       return (<h2>Gracias por tu compra !! Te dejamos tu número de pedido por cualquier consulta que quiera realizar. {orderId} </h2>);
     }
@@ -62,7 +84,23 @@ const Form = () => {
     <label className="form-label">APELLIDO</label>
     <input onChange={handleLastName} value={lastName} placeholder='Ingrese su apellido..' type="text" className="form-control" name='apellido' required/>
   </div>
-  <button type="submit" className="btn btn-dark">ENVIAR</button>
+  <div className="mb-3">
+    <label className="form-label">TELEFONO</label>
+    <input onChange={handleTelefono} value={telefono} placeholder='Ingrese su telefono..' type="number" className="form-control" name='telefono' required/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">DIRECCIÓN</label>
+    <input onChange={handleDireccion} value={direccion} placeholder='Ingrese su dirección..' type="text" className="form-control" name='direccion' required/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">EMAIL</label>
+    <input onChange={handleMailUno} value={mailUno} placeholder='Ingrese su email..' type="text" className="form-control" name='email' required/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">EMAIL</label>
+    <input onChange={handleMailDos} value={mailDos} placeholder='Ingrese nuevamente su email..' type="text" className="form-control" name='email' required/>
+  </div>
+  <button disabled={true ? mailUno !== mailDos : mailUno === mailDos} type="submit" className="btn btn-dark">ENVIAR</button>
 </form>
   )
 }
