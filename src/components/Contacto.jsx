d4b9484199ea1d09ac23bf9bdd7262b6b98dd3bd
Santiago Fiorotto <santifiorotto@hotmail.com>
2022-11-26 23:32:13 -0300

    ultimos cambios

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import {ImWhatsapp} from "react-icons/im"
+
 
 const Contacto = () => {
   return (
     <div className='container'>
-          <div className='container'>
-          <h1>Contactanos !</h1>
-          <h2>3534277821</h2>
-            </div>
+      <div className='ubi'>
+        <h3 className='ubi-text container'>Encontranos en Martinez Mendoza 652, Villa María, Córdoba.</h3>
+      <img className='ubi-foto container' src="https://res.cloudinary.com/djjmhiwzd/image/upload/v1669424589/Captura_de_Pantalla_2022-11-25_a_la_s_22.01.24_kvkeu4.png" alt="Localización FIKA" />
+      <h3 className='ubi-text container'>O comunicate al 3534277821<ImWhatsapp/></h3>
+      </div>
+          
     </div>
 
   )
--- src/components/carrito/Cart.jsx
+++ src/components/carrito/Cart.jsx
@@ -12,20 +12,23 @@ const Cart = () => {
   return (
     <div>
       {cart.map((prod) =>(
-        <div key={prod.id} className='col'>
-        <div className="card ">
+        <div key={prod.id} className='container carrito-contenedor'>
+        <div className="card carrito-vista">
    <img src={prod.img} className="card-img-top" alt={prod.title}/>
    <div className="card-body">
      <h4 className="card-title productoDescripcion">{prod.title}</h4>
      <h4 className="card-title productoDescripcion"> {prod.cantidad}</h4>
-     <button onClick={()=>deleteOne(prod.id)}>Eliminar</button>
+     <button className='btn btn-danger' onClick={()=>deleteOne(prod.id)}>Eliminar</button>
    </div>
  </div>
      </div>
       ))}
-      <h2>TOTAL: ${totalPrecio()}</h2>
+      <h2 className='precio-carrito'>TOTAL: ${totalPrecio()}</h2>
+      <div className='botones-carrito'>
       <button className="btn btn-danger"onClick={deleteAll}>Vaciar Carrito</button>
-      <Link className="btn btn-dark" to="/checkout">CHECKOUT</Link>
+      <Link className="btn btn-success" to="/checkout">CHECKOUT</Link>
+      </div>
+
     </div>
   )
 }
